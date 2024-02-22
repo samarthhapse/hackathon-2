b14da845c1a4d27a45a1ddb13f68f2516e1c96be
Samarth Hapse <131296047+samarthhapse@users.noreply.github.com>
2024-02-22 16:14:52 +0530

    Update main.py

--- FastAPI_Backend/main.py
+++ FastAPI_Backend/main.py
@@ -4,7 +4,7 @@ from typing import List,Optional
 import pandas as pd
 from model import recommend,output_recommended_recipes
 
-dataset=pd.read_csv('../Data/dataset.csv',compression='gzip')
+dataset=pd.read_csv('../Data/dataset.csv')
 
 app = FastAPI()
 
